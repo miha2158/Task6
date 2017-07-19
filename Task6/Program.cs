5ddfa3f8d24da7f6c51e0874d52ae827511ad662
miha2158 <miha.vanya@gmail.com>
2017-07-20 00:53:42 +0500

    Написаны функции

--- Task6/Program.cs
+++ Task6/Program.cs
@@ -8,8 +8,26 @@ namespace Task6
 {
     class Program
     {
+        static void Swap(ref double a, ref double b)
+        {
+            var temp = a;
+            a = b;
+            b = temp;
+        }
+
+        static void iterrate(ref double a, ref double b, ref double c)
+        {
+            double result = a + b / 3 + c * 3;
+            c = b;
+            b = a;
+            a = result;
+        }
+        
         static void Main(string[] args)
         {
+
+
+
         }
     }
 }
